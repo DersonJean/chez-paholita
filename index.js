22ae7a1fafdfa9daf3f554840d275a653c46a160
DersonJean <derson.jean@estudante.uffs.edu.br>
2025-07-04 19:40:42 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+<script>
+  document.querySelector('.menu-toggle')
+    .addEventListener('click', () =>
+      document.querySelector('.main-menu')
+              .classList.toggle('open'));
+</script>
